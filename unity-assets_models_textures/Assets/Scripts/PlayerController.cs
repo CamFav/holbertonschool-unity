b10f0709b847bf62206e1bb95dc51f2d2a4d373b
Camille F <camillefvrl@gmail.com>
2024-06-03 21:09:30 +0200

    [T4] Camera following Player

---
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpForce = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Check if the player is on the ground
+        if (controller.isGrounded)
+        {
+            // Get input from WASD keys
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            // Create a vector based on the input
+            moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            // Check for jump input
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Move the player
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
